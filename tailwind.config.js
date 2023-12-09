cfe2776a30e4395b46a6367312d55d33f9f17d57
abcd_arl <lingga.abdulrahman.u@gmail.com>
2023-12-09 08:51:20 +0800

    added animations and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    animation: {
+      pulse: "pulse 1s ease-in-out infinite",
+      spin: "spin 1s linear infinite",
+      "fade-in": "fadeIn 1s ease-out",
+      "roll-out": "rollOut .4s ease-out",
+      "roll-in": "rollIn .4s ease-out",
+    },
+    extend: {
+      colors: {
+        "rust-gray": "#47585C",
+        "willow-gray": "#C8D5BB",
+      },
+    },
   },
   plugins: [],
 };
