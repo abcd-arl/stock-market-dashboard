80380c13350b60f7cff6e6d787e27e4213a6f185
abcd_arl <lingga.abdulrahman.u@gmail.com>
2023-12-09 08:52:12 +0800

    added skeleton loading

--- src/components/marketNews.jsx
+++ src/components/marketNews.jsx
@@ -1,4 +1,5 @@
 import { useGetMarketNewsQuery } from "../redux/finnhub";
+import SkeletonLoading from "./skeletonLoading";
 
 export default function MarketNews() {
   const { data, error, isLoading } = useGetMarketNewsQuery();
@@ -19,29 +20,65 @@ export default function MarketNews() {
     return date.toLocaleString();
   };
 
-  if (isLoading) return <div>Loading...</div>;
-
   return (
-    <div className="rounded border px-8 py-4 text-[0.8rem]">
-      <h2 className="mb-4 text-2xl font-bold">Market News</h2>
-      <div className="grid grid-cols-4 gap-6">
-        {data.map((article) => (
-          <a
-            key={article.id}
-            href={article.url}
-            target="_blank"
-            className="w-fit"
-          >
-            <div className="space-y-1.5">
-              <img src={article.image} className="h-40 w-96 object-cover" />
-              <h3 className="text-lg font-bold">{article.headline}</h3>
-              <p className="text-gray-500">
-                {article.source} | {formatDatetime(article.datetime)}
-              </p>
-              <p className="text-sm">{article.summary}</p>
-            </div>
-          </a>
-        ))}
+    <div className="rounded-md border border-gray-300 px-8 py-4 text-[0.8rem]">
+      <h2 className="mb-6 text-2xl font-bold">Market News</h2>
+      <div className="flex flex-col gap-5 pr-16">
+        {isLoading &&
+          Array(3)
+            .fill(0)
+            .map((_, i) => (
+              <div
+                key={i}
+                className="w-full border-b border-gray-200 pb-5 last:border-b-0 hover:text-zinc-500"
+              >
+                <div className="flex gap-8">
+                  <div className="mb-2 basis-[10%] text-gray-500">
+                    <SkeletonLoading className="h-2 w-full" />
+                  </div>
+                  <div className="basis-[70%] space-y-5">
+                    <div className="space-y-2">
+                      <SkeletonLoading className="mb1 h-2 w-[80%]" />
+                      <SkeletonLoading className="h-2 w-24" />
+                    </div>
+                    <div className="space-y-2">
+                      <SkeletonLoading className="h-2 w-full" />
+                      <SkeletonLoading className="h-2 w-full" />
+                      <SkeletonLoading className="h-2 w-full" />
+                      <SkeletonLoading className="h-2 w-[60%]" />
+                    </div>
+                  </div>
+                  <SkeletonLoading className="h-36 w-72" />
+                </div>
+              </div>
+            ))}
+        {!isLoading &&
+          data.map((article) => (
+            <a
+              key={article.id}
+              href={article.url}
+              target="_blank"
+              className="w-full border-b border-gray-200 pb-5 last:border-b-0 hover:text-zinc-500"
+            >
+              <div className="flex gap-8">
+                <p className="mb-2 basis-[15%] text-gray-500">
+                  {formatDatetime(article.datetime)}
+                </p>
+                <div className="basis-[70%] space-y-3">
+                  <h3 className="mb-1 text-base font-bold">
+                    {article.headline.startsWith(":")
+                      ? article.headline.slice(1).trim()
+                      : article.headline}
+                  </h3>
+                  <p className="mb-2 basis-[15%] text-gray-500">
+                    {formatDatetime(article.datetime)}
+                  </p>
+                  <p className="text-sm">{article.summary}</p>
+                </div>
+                <img src={article.image} className="h-36 w-64 object-cover" />
+              </div>
+            </a>
+          ))}
       </div>
     </div>
   );
--- src/components/watchList.jsx
+++ src/components/watchList.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
 import { useLazyGetProfileAndQuoteQuery } from "../redux/finnhub";
 import { SOCKET_URL } from "../App";
+import useWebSocket from "react-use-websocket";
 import closeButton from "../assets/icons8-close.svg";
+import SkeletonLoading from "./skeletonLoading";
 
 export default function WatchList({ symbols, topAndTrendingTickers }) {
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
-    SOCKET_URL,
-    { share: true },
-  );
-  const [trigger, result] = useLazyGetProfileAndQuoteQuery();
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(SOCKET_URL, {
+    share: true,
+  });
   const [trades, setTrades] = useState({});
+  const [trigger, result] = useLazyGetProfileAndQuoteQuery();
   const [symbolsData, setSymbolsData] = useState({});
   const [inputValue, setInputValue] = useState("");
+  const [animation, setAnimation] = useState(null);
   const [shouldSave, setShouldSave] = useState({
     initial: false,
     final: false,
@@ -70,10 +71,9 @@ export default function WatchList({ symbols, topAndTrendingTickers }) {
     }
   }, [lastJsonMessage]);
 
-  if (result.isLoading) return <div>Loading...</div>;
   return (
-    <div className="w-full rounded border pb-6 pt-4 text-[0.8rem]">
-      <h2 className="mb-6 px-4 text-2xl font-bold">Your Watch List</h2>
+    <div className="sticky top-2 w-full rounded-lg border border-gray-300 pb-6 pt-4 text-[0.8rem] shadow">
+      <h2 className="mb-3 px-4 text-2xl font-bold">Your Watch List</h2>
       <form onSubmit={addSymbol} className="relative mb-4 flex w-full px-4">
         <input
           type="text"
@@ -81,16 +81,16 @@ export default function WatchList({ symbols, topAndTrendingTickers }) {
           placeholder="Add a symbol"
           disabled={result.isFetching}
           onChange={(e) => setInputValue(e.target.value.toUpperCase())}
-          className="m-auto block w-full rounded border border-gray-200 py-1.5 pl-2 pr-10 text-sm"
+          className="m-auto block w-full rounded-md border border-gray-300 py-1.5 pl-2 pr-10 text-sm shadow-sm"
         />
-        {result.isFetching ? (
+        {result.isFetching && !result.isLoading ? (
           <div
             role="status"
             className="absolute bottom-1 right-5 top-1.5 block px-2"
           >
             <svg
               aria-hidden="true"
-              className="inline h-5 w-5 animate-spin fill-green-500 text-gray-200 dark:text-gray-300"
+              className="fill-rust-gray inline h-5 w-5 animate-spin text-gray-200 dark:text-gray-300"
               viewBox="0 0 100 101"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -110,66 +110,92 @@ export default function WatchList({ symbols, topAndTrendingTickers }) {
           <input
             type="submit"
             value="GO"
-            className="absolute bottom-0 right-5 top-0 px-2"
+            className="absolute bottom-0 right-5 top-0 cursor-pointer p-2"
           />
         )}
       </form>
       <div className="mx-2">
-        {Object.keys(symbolsData).map((symbol) => {
-          const name = symbolsData[symbol].profile.name;
-          const logo = symbolsData[symbol].profile.logo;
-          const close = symbolsData[symbol].quote.pc.toFixed(3);
-
-          const current = trades[symbol]?.p
-            ? trades[symbol]?.p.toFixed(3)
-            : symbolsData[symbol].quote.c.toFixed(3);
-          const change = (current - close).toFixed(3);
-          const percentChange = ((change / close) * 100).toFixed(3);
-
-          return (
-            <div
-              key={symbol}
-              className="group relative flex justify-between rounded p-2 hover:bg-gray-50"
-            >
-              <button
-                className="absolute -right-1.5 -top-2 hidden group-hover:block"
-                onClick={() => removeSymbol(symbol)}
-              >
-                <img src={closeButton} alt="" className="h-5 w-5" />
-              </button>
-              <div className="flex items-center">
-                {change < 0 ? (
-                  <div className="mr-3 text-lg text-red-600">↓</div>
-                ) : (
-                  <div className="mr-3 text-lg text-green-600">↑</div>
-                )}
-
-                <img
-                  src={logo}
-                  alt={name}
-                  className="mr-3 h-7 w-7 rounded-full"
-                />
-                <div className="flex flex-col">
-                  <span className="font-bold">{symbol}</span>
-                  <span>{name}</span>
+        {result.isLoading &&
+          Array(10)
+            .fill()
+            .map((_, index) => (
+              <div key={index} className="flex justify-between px-2 py-3">
+                <div className="flex items-center">
+                  <SkeletonLoading className="mr-4 h-2.5 w-2.5" />
+                  <SkeletonLoading className="mr-3 h-7 w-7 rounded-full" />
+                  <div className="flex flex-col gap-2">
+                    <SkeletonLoading className="h-2 w-24" />
+                    <SkeletonLoading className="h-2 w-32" />
+                  </div>
+                </div>
+                <div className="flex flex-col items-end justify-center gap-2">
+                  <div>
+                    <SkeletonLoading className="h-2 w-24" />
+                  </div>
+                  <div className="flex gap-2">
+                    <SkeletonLoading className="h-2 w-16" />
+                    <SkeletonLoading className="h-2 w-16" />
+                  </div>
                 </div>
               </div>
+            ))}
+        {!result.isLoading &&
+          Object.keys(symbolsData).map((symbol, index) => {
+            const name = symbolsData[symbol].profile.name;
+            const logo = symbolsData[symbol].profile.logo;
+            const close = symbolsData[symbol].quote.pc.toFixed(3);
+
+            const current = trades[symbol]?.p
+              ? trades[symbol]?.p.toFixed(3)
+              : symbolsData[symbol].quote.c.toFixed(3);
+            const change = (current - close).toFixed(3);
+            const percentChange = ((change / close) * 100).toFixed(3);
+
+            return (
               <div
-                className={`text-right font-mono ${
-                  change < 0 ? "text-red-600" : "text-green-600"
-                }`}
+                key={symbol}
+                className={`animate-roll-out group relative flex justify-between rounded p-2 transition-all hover:bg-gray-50`}
               >
-                <h3>
-                  {current}
-                  <span className="invisible">)</span>
-                </h3>
-                <p>
-                  {percentChange}% ({change})
-                </p>
+                <button
+                  className="absolute -right-1.5 -top-2 hidden group-hover:block"
+                  onClick={() => removeSymbol(symbol)}
+                >
+                  <img src={closeButton} alt="" className="h-5 w-5" />
+                </button>
+                <div className="flex items-center justify-center">
+                  {change < 0 ? (
+                    <div className="mr-3 text-lg text-red-600">↓</div>
+                  ) : (
+                    <div className="mr-3 text-lg text-green-600">↑</div>
+                  )}
+                  <img
+                    src={logo}
+                    alt={name}
+                    className="mr-3 h-7 w-7 rounded-full"
+                  />
+                  <div className="w-full">
+                    <p className="font-bold">{symbol}</p>
+                    <p className="w-44 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                      {name}
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className={`text-right font-mono ${
+                    change < 0 ? "text-red-600" : "text-green-600"
+                  }`}
+                >
+                  <div key={"current" + current} className="animate-fade-in">
+                    {current}
+                    <span className="invisible">)</span>
+                  </div>
+                  <p>
+                    {percentChange}% ({change})
+                  </p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
