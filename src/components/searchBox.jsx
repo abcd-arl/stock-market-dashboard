2105834ae9750270509eea2bc8e10554a9b9f99b
abcd_arl <lingga.abdulrahman.u@gmail.com>
2023-12-13 20:38:54 +0800

    update

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -1,6 +1,20 @@
+import { useLocation } from "wouter";
+import { useState } from "react";
+
 export default function SearchBox() {
+  const [, setLocation] = useLocation();
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <div className="relative">
+    <form
+      className="relative"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const symbol = e.target[0].value;
+        setInputValue("");
+        setLocation(`/profile/${symbol}`);
+      }}
+    >
       <div className="absolute inset-y-0 left-0 flex items-center pl-2">
         <svg
           className="h-4 w-4 text-gray-500"
@@ -20,8 +34,10 @@ export default function SearchBox() {
       <input
         className="block w-full rounded-md border border-gray-300 bg-transparent py-2 pl-8 pr-2 text-sm shadow focus:border-zinc-500 focus:ring-zinc-500"
         type="text"
-        placeholder="Search"
+        placeholder="Search a symbol"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value.toUpperCase())}
       />
-    </div>
+    </form>
   );
 }
