af420ffa4265bc21c389729b2e345ec42b0e3cc1
abcd_arl <lingga.abdulrahman.u@gmail.com>
2023-12-13 20:39:04 +0800

    handled some errors

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,28 @@
 import TricketTable from "../components/tricketTable";
 import MarketNews from "../components/marketNews";
+import Error from "./error";
 
 export default function Home() {
   const topTrickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA"];
   const trendingTrickers = ["NIO", "GTLB", "JNJ", "NVDA", "AMD", "DIS"];
 
+  function displayError(type) {
+    return <Error type={type} />;
+  }
+
   return (
     <>
-      <TricketTable title={"Top Tickers"} companies={topTrickers} />
-      <TricketTable title={"Trending Tickers"} companies={trendingTrickers} />
-      <MarketNews />
+      <TricketTable
+        title={"Top Tickers"}
+        companies={topTrickers}
+        displayError={displayError}
+      />
+      <TricketTable
+        title={"Trending Tickers"}
+        companies={trendingTrickers}
+        displayError={displayError}
+      />
+      <MarketNews displayError={displayError} />
     </>
   );
 }
