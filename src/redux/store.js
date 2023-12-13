30dcdaaaea9de61f5c69c71d132ac430cdc1c6c7
abcd_arl <lingga.abdulrahman.u@gmail.com>
2023-12-13 20:36:45 +0800

    added alphavantage api

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { finnhubApi } from "./finnhub";
+import { alphavantageAPI } from "./alphavantage";
 
 export const store = configureStore({
   reducer: {
     [finnhubApi.reducerPath]: finnhubApi.reducer,
+    [alphavantageAPI.reducerPath]: alphavantageAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(finnhubApi.middleware),
+    getDefaultMiddleware().concat(
+      finnhubApi.middleware,
+      alphavantageAPI.middleware,
+    ),
 });
