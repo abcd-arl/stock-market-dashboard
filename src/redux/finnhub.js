0025277e9468055c037d9bc08481165a1ed31598
abcd_arl <lingga.abdulrahman.u@gmail.com>
2023-12-13 20:39:32 +0800

    added endpoints

--- src/redux/finnhub.js
+++ src/redux/finnhub.js
@@ -14,45 +14,99 @@ export const finnhubApi = createApi({
     getProfile: builder.query({
       query: (symbol) => `stock/profile2?symbol=${symbol}&token=${TOKEN}`,
     }),
+    getPeers: builder.query({
+      query: (params) =>
+        `stock/peers?symbol=${params.symbol}&grouping=${params.grouping}&token=${TOKEN}`,
+    }),
     getMarketNews: builder.query({
       query: () => `news?category=general&token=${TOKEN}`,
     }),
-    getProfileAndQuote: builder.query({
+    getCompanyNews: builder.query({
+      query: (param) =>
+        `company-news?symbol=${param.symbol}&from=${param.from}&to=${param.to}&token=${TOKEN}`,
+    }),
+    getBasicFinancials: builder.query({
+      query: (symbol) =>
+        `stock/metric?symbol=${symbol}&metric=all&token=${TOKEN}`,
+    }),
+    getProfilesAndQuotes: builder.query({
       queryFn: async (arg, queryApi) => {
         const data = {};
         for (const symbol of arg) {
           const profile = await queryApi.dispatch(
-            finnhubApi.endpoints.getProfile.initiate(symbol)
+            finnhubApi.endpoints.getProfile.initiate(symbol),
           );
           const quote = await queryApi.dispatch(
-            finnhubApi.endpoints.getQuote.initiate(symbol)
+            finnhubApi.endpoints.getQuote.initiate(symbol),
           );
 
+          if (profile?.error?.status === 429 || quote?.error?.status === 429)
+            return { error: { status: 429 } };
+
           data[symbol] = {
-            profile: profile.data ? profile.data : profile.error,
-            quote: quote.data ? quote.data : quote.error,
+            profile: profile.data,
+            quote: quote.data,
           };
         }
         return { data };
       },
     }),
-    getQuotes: builder.query({
+    getProfileAndQuote: builder.query({
       queryFn: async (arg, queryApi) => {
-        const quotes = {};
-        for (const symbol of arg) {
-          const result = await queryApi.dispatch(
-            finnhubApi.endpoints.getQuote.initiate(symbol)
+        const profile = await queryApi.dispatch(
+          finnhubApi.endpoints.getProfile.initiate(arg),
+        );
+        const quote = await queryApi.dispatch(
+          finnhubApi.endpoints.getQuote.initiate(arg),
+        );
+
+        return {
+          data: {
+            profile: profile.data ? profile.data : profile.error,
+            quote: quote.data ? quote.data : quote.error,
+          },
+        };
+      },
+    }),
+    getPeersProfilesAndQuotes: builder.query({
+      queryFn: async (arg, queryApi) => {
+        const peers = await queryApi.dispatch(
+          finnhubApi.endpoints.getPeers.initiate({
+            symbol: arg.symbol,
+            grouping: arg.grouping,
+          }),
+        );
+
+        const data = {};
+        for (const symbol of peers.data) {
+          const profile = await queryApi.dispatch(
+            finnhubApi.endpoints.getProfile.initiate(symbol),
           );
-          quotes[symbol] = result.data ? result.data : result.error;
+          const quote = await queryApi.dispatch(
+            finnhubApi.endpoints.getQuote.initiate(symbol),
+          );
+
+          if (profile.error || quote.error) continue;
+
+          data[symbol] = {
+            profile: profile.data,
+            quote: quote.data,
+          };
         }
-        return { data: quotes };
+        return { data };
       },
     }),
   }),
 });
 
 export const {
+  useGetProfilesAndQuotesQuery,
+  useLazyGetProfilesAndQuotesQuery,
   useGetProfileAndQuoteQuery,
-  useLazyGetProfileAndQuoteQuery,
   useGetMarketNewsQuery,
+  useGetCompanyNewsQuery,
+  useGetBasicFinancialsQuery,
+  useGetPeersProfilesAndQuotesQuery,
+  useGetProfileQuery,
+  useGetPeersQuery,
 } = finnhubApi;
