dcef91f4e7d8c12c66a39bd5ea24e31c3e109d1c
abcd_arl <lingga.abdulrahman.u@gmail.com>
2023-12-18 18:14:11 +0800

    update to table

--- src/components/relatedTickers.jsx
+++ src/components/relatedTickers.jsx
@@ -1,3 +1,5 @@
+import { Link } from "wouter";
+import { formatMarketCap } from "../utils/format";
 import { useGetPeersProfilesAndQuotesQuery } from "../redux/finnhub";
 
 export default function RelatedTickers({ symbol }) {
@@ -9,37 +11,96 @@ export default function RelatedTickers({ symbol }) {
   if (isLoading || isFetching) return <div>Loading...</div>;
   if (error) return <div>Oh no, there was an error</div>;
 
+  console.log(data);
+
   return (
-    <div className="">
-      <h3 className="mb-5 text-2xl font-bold">Related Tickers</h3>
-      <div className="grid grid-cols-3 gap-x-6 gap-y-2">
-        {Object.keys(data).map((symbol) => {
-          const name = data[symbol].profile.name;
-          const logo = data[symbol].profile.logo;
-          const close = data[symbol].quote.pc.toFixed(3);
+    <div className="mt-10">
+      <h3 className="mb-2 text-xs font-bold uppercase text-zinc-500">
+        Companies on the Same Industry
+      </h3>
+      <table className="w-full border-collapse text-sm">
+        <thead>
+          <tr className="p-10 text-right font-semibold text-zinc-400">
+            <th className="py-2.5 pl-3 text-left">Company</th>
+            <th>Current</th>
+            <th>Change</th>
+            <th>Change %</th>
+            <th>High</th>
+            <th>Low</th>
+            <th>Open</th>
+            <th>Close</th>
+            <th className="pr-3">Market Cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data).map((symbol) => {
+            const {
+              logo,
+              name,
+              marketCapitalization: marketCap,
+            } = data[symbol].profile;
+            const {
+              c: current,
+              d: change,
+              dp: percentChange,
+              h: high,
+              l: low,
+              o: open,
+              pc: close,
+            } = data[symbol].quote;
 
-          const current = data[symbol].quote.c.toFixed(3);
-          const change = (current - close).toFixed(3);
-          const percentChange = ((change / close) * 100).toFixed(3);
-
-          return (
-            <div
-              key={symbol}
-              className={`flex w-full items-center justify-between rounded border p-2 text-sm`}
-            >
-              <div className="flex items-center gap-2.5">
-                <img src={logo} alt={name} className="h-8 w-8 rounded-full" />
-                <p className="font-bold">{symbol}</p>
-              </div>
-              <p
-                className={`${change < 0 ? "text-red-600" : "text-green-600"}`}
+            return (
+              <tr
+                key={symbol}
+                className="rounded-full border-y border-gray-100 text-right font-mono last:border-0 hover:bg-gray-50"
               >
-                <span className="font-mono">{current}</span> USD
-              </p>
-            </div>
-          );
-        })}
-      </div>
+                <td className="w-36 py-2 pl-3 text-left font-sans">
+                  <Link
+                    to={`/profile/${symbol}`}
+                    className="flex w-fit items-center"
+                  >
+                    <div className="mr-2 h-6 shrink-0 basis-6 rounded-full bg-gray-200">
+                      <img
+                        src={logo}
+                        alt={name}
+                        className="h-6 w-6 rounded-full"
+                      />
+                    </div>
+                    <div className="flex w-fit gap-2">
+                      <h3 className="font-bold">{symbol}</h3>
+                      <p className="w-36 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                        {name}
+                      </p>
+                    </div>
+                  </Link>
+                </td>
+                <td>{current?.toFixed(3)}</td>
+                <td
+                  className={`sm:pr-0 ${
+                    change < 0 ? "text-red-600" : "text-green-600"
+                  }`}
+                >
+                  {change > 0 ? "+" : ""}
+                  {change.toFixed(3)}
+                </td>
+                <td
+                  className={`pr-3 sm:pr-0 ${
+                    percentChange < 0 ? "text-red-600" : "text-green-600"
+                  }`}
+                >
+                  {change > 0 ? "+" : ""}
+                  {percentChange.toFixed(3)}
+                </td>
+                <td>{high.toFixed(3)}</td>
+                <td>{low.toFixed(3)}</td>
+                <td>{open.toFixed(3)}</td>
+                <td>{close.toFixed(3)}</td>
+                <td className="pr-3">{formatMarketCap(marketCap)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
